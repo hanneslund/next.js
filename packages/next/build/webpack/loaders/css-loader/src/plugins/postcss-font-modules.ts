00b2bcd8a87fd316f6185aec81f0d8f1b87b27b2
Hannes Bornö <hannes.borno@vercel.com>
2022-07-14 09:17:54 +0200

    wip

--- packages/next/build/webpack/loaders/css-loader/src/plugins/postcss-font-modules.ts
+++ packages/next/build/webpack/loaders/css-loader/src/plugins/postcss-font-modules.ts
@@ -19,9 +19,9 @@ const plugin = (exports: any[]) => {
       // Replace font-family in @font-face and CLASSES?
       let rawFamily: string | undefined
 
-      let family: string | undefined
-      let style: string | undefined
-      let weight: string | undefined
+      let fontFamily: string | undefined
+      let fontStyle: string | undefined
+      let fontWeight: string | undefined
       for (const node of root.nodes) {
         if (node.type === 'atrule' && node.name === 'font-face') {
           for (const decl of node.nodes) {
@@ -34,29 +34,29 @@ const plugin = (exports: any[]) => {
               }
               rawFamily = decl.value
 
-              let fontFamily = decl.value
-              if (fontFamily[0] === "'" || fontFamily[0] === '"') {
-                fontFamily = fontFamily.slice(1, fontFamily.length - 1)
+              let family = decl.value
+              if (family[0] === "'" || family[0] === '"') {
+                family = family.slice(1, family.length - 1)
               }
-              fontFamily = `'${fontFamily}-${hashString}'`
+              family = `'${family}-${hashString}'`
 
-              decl.value = fontFamily
-              family = fontFamily
+              decl.value = family
+              fontFamily = family
             } else if (decl.prop === 'font-weight') {
-              if (weight && decl.value !== weight) {
+              if (fontWeight && decl.value !== fontWeight) {
                 throw decl.error(
                   'A @font-face module may only contain one font weight'
                   // { word: 'font-weight' }
                 )
               }
-              weight = decl.value
+              fontWeight = decl.value
             } else if (decl.prop === 'font-style') {
-              if (style && decl.value !== style) {
+              if (fontStyle && decl.value !== fontStyle) {
                 throw decl.error(
                   'A @font-face module may only contain one font style'
                 )
               }
-              style = decl.value
+              fontStyle = decl.value
             }
           }
         }
@@ -65,21 +65,31 @@ const plugin = (exports: any[]) => {
       // Add font class
       const classRule = new postcss.Rule({ selector: '.className' })
       const declarations = [
-        new postcss.Declaration({ prop: 'font-family', value: family }),
-        ...(style
-          ? [new postcss.Declaration({ prop: 'font-style', value: style })]
+        new postcss.Declaration({ prop: 'font-family', value: fontFamily }),
+        ...(fontStyle
+          ? [new postcss.Declaration({ prop: 'font-style', value: fontStyle })]
           : []),
-        ...(weight
-          ? [new postcss.Declaration({ prop: 'font-weight', value: weight })]
+        ...(fontWeight
+          ? [
+              new postcss.Declaration({
+                prop: 'font-weight',
+                value: fontWeight,
+              }),
+            ]
           : []),
       ]
       classRule.nodes = declarations
       root.nodes.push(classRule)
 
       // Export @font-face values
-      exports.push({ name: 'family', value: family })
-      exports.push({ name: 'style', value: style })
-      exports.push({ name: 'weight', value: weight })
+      exports.push({
+        name: 'style',
+        value: {
+          fontFamily,
+          ...(fontStyle ? { fontStyle } : {}),
+          ...(fontWeight ? { fontWeight } : {}),
+        },
+      })
     },
   }
 }
