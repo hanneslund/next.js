6362e9bdf56e939c97683ec46f23ef96e8e63d99
Hannes Bornö <hannes.borno@vercel.com>
2022-08-13 10:22:28 +0200

    wip

---
+++ packages/next-swc/crates/core/tests/fixture/next-font-downloaders/import-as/input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Acme as a } from 'cool-fonts'
+
+const acme1 = a({
+  variant: '400',
+})
+
+const acme2 = a({
+  variant: '400',
+})
---
+++ packages/next-swc/crates/core/tests/fixture/next-font-downloaders/import-as/output.js
@@ -0,0 +1,3 @@
+import acme1 from 'cool-fonts?{"font":"a","variant":"400"}';
+import acme2 from 'cool-fonts?{"font":"a","variant":"400"}';
+import React from 'react';
