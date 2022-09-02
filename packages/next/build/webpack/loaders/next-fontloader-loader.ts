c35aafb332c826fef20aa01d27a569d2cb62d385
Hannes Bornö <hannes.borno@vercel.com>
2022-09-02 11:25:35 +0200

    wip

--- packages/next/build/webpack/loaders/next-fontloader-loader.ts
+++ packages/next/build/webpack/loaders/next-fontloader-loader.ts
@@ -2,39 +2,44 @@ import path from 'path'
 import loaderUtils from 'next/dist/compiled/loader-utils3'
 
 export default async function nextFontLoader(this: any) {
-  const callback = this.async()
-  const { isServer, assetPrefix, fontLoaderOptions } = this.getOptions()
+  const fontLoaderSpan = this.currentTraceSpan.traceChild(
+    'next-fontloader-loader'
+  )
+  return fontLoaderSpan.traceAsyncFn(async () => {
+    const callback = this.async()
+    const { isServer, assetPrefix, fontLoaderOptions } = this.getOptions()
 
-  const emitFontFile = (content: Buffer, ext: string, preload: true) => {
-    const opts = { context: this.rootContext, content }
-    const interpolatedName = loaderUtils.interpolateName(
-      this,
-      // Font files ending with .p.[ext] will be preloaded
-      `static/fonts/[hash]${preload ? '.p' : ''}.${ext}`,
-      opts
-    )
-    const outputPath = `${assetPrefix}/_next/${interpolatedName}`
-    if (!isServer) {
-      this.emitFontFile(interpolatedName, content, null)
+    const emitFontFile = (content: Buffer, ext: string, preload: true) => {
+      const opts = { context: this.rootContext, content }
+      const interpolatedName = loaderUtils.interpolateName(
+        this,
+        // Font files ending with .p.[ext] will be preloaded
+        `static/fonts/[hash]${preload ? '.p' : ''}.${ext}`,
+        opts
+      )
+      const outputPath = `${assetPrefix}/_next/${interpolatedName}`
+      if (!isServer) {
+        this.emitFile(interpolatedName, content, null)
+      }
+      return outputPath
     }
-    return outputPath
-  }
 
-  // next-swc next_font_loaders turns each function call argument into JSON seperated by a semicolon
-  let [functionName, ...args] = this.resourceQuery.slice(1).split(';')
-  args = args.map((value: string) => JSON.parse(value))
+    // next-swc next_font_loaders turns each function call argument into JSON seperated by a semicolon
+    let [functionName, ...args] = this.resourceQuery.slice(1).split(';')
+    args = args.map((value: string) => JSON.parse(value))
 
-  const loader = require(path.join(this.resourcePath, '../loader.js'))
-  try {
-    const css = await loader.default(
-      functionName,
-      args,
-      fontLoaderOptions,
-      emitFontFile
-    )
-    callback(null, css)
-  } catch (err: any) {
-    err.stack = false
-    callback(err)
-  }
+    const loader = require(path.join(this.resourcePath, '../loader.js'))
+    try {
+      const css = await loader.default(
+        functionName,
+        args,
+        fontLoaderOptions,
+        emitFontFile
+      )
+      callback(null, css)
+    } catch (err: any) {
+      err.stack = false
+      callback(err)
+    }
+  })
 }
