13781bc9e621dbcf3770fd34730069214db7e27d
Hannes Bornö <hannes.borno@vercel.com>
2022-09-05 08:44:54 +0200

    wip

--- test/development/next-font/missing-subsets-in-google-loader/next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  experimental: {
-    fontLoaders: {
-      '@next/font/google': {},
-    },
-  },
-}
--- test/development/next-font/missing-subsets-in-google-loader/pages/index.js
+++
@@ -1,7 +0,0 @@
-import { ABeeZee } from '@next/font/google'
-
-const abeezee = ABeeZee()
-
-export default function Index() {
-  return <p className={abeezee.className}>Hello world</p>
-}
