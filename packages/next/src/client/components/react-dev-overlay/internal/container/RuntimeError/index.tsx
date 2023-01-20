a9f10edd4149ed76feaf6d4cd7c44b09b6e68915
Hannes Bornö <hannes.borno@vercel.com>
2023-01-20 13:59:00 +0100

    clean

--- packages/next/src/client/components/react-dev-overlay/internal/container/RuntimeError/index.tsx
+++ packages/next/src/client/components/react-dev-overlay/internal/container/RuntimeError/index.tsx
@@ -5,58 +5,13 @@ import { noop as css } from '../../helpers/noop-template'
 import { OriginalStackFrame } from '../../helpers/stack-frame'
 import { groupStackFramesByFramework } from '../../helpers/group-stack-frames-by-framework'
 import { ComponentStackFrame } from '../../helpers/parse-component-stack'
-import { useOpenInEditor } from '../../helpers/use-open-in-editor'
 import { CallStackFrame } from './CallStackFrame'
 import { GroupedStackFrames } from './GroupedStackFrames'
+import { ComponentStackFrameRow } from './ComponentStackFrameRow'
 
 export type RuntimeErrorProps = {
   error: ReadyRuntimeError
   componentStackFrames?: ComponentStackFrame[]
-}
-
-function ComponentStackFrameRow({
-  componentStackFrame: { component, file, lineNumber, column },
-}: {
-  componentStackFrame: ComponentStackFrame
-}) {
-  const open = useOpenInEditor({
-    file,
-    column,
-    lineNumber,
-  })
-
-  return (
-    <div data-nextjs-component-stack-frame>
-      <h6>{component}</h6>
-      {file ? (
-        <div
-          tabIndex={10} // match CallStackFrame
-          role={'link'}
-          onClick={open}
-          title={'Click to open in your editor'}
-        >
-          <span>
-            {file} ({lineNumber}:{column})
-          </span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-            <polyline points="15 3 21 3 21 9"></polyline>
-            <line x1="10" y1="14" x2="21" y2="3"></line>
-          </svg>
-        </div>
-      ) : (
-        <div />
-      )}
-    </div>
-  )
 }
 
 const RuntimeError: React.FC<RuntimeErrorProps> = function RuntimeError({
